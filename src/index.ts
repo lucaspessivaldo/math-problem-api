8981df71d304bcd3a27f20fd19d191c9f98c12d7
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-10-27 18:45:32 -0300

    add - cors

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
 import apiRouter from './router/api.router'
 
 const app: Express = express()
 const port = 5000
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api', apiRouter)
-
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.json({
-    message: 'Server is running'
+    message: 'MATH PROBLEM API'
   })
 })
 
